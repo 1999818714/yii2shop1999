2aa6c4ac7806bc890766a9058a08ffc3aefa8dde
1999818714 <2039236621@qq.com>
2018-02-26 22:12:44 +0800

    2.26晚上

--- console/migrations/m180226_111549_create_brand_table.php
+++ console/migrations/m180226_111549_create_brand_table.php
@@ -12,8 +12,13 @@ class m180226_111549_create_brand_table extends Migration
      */
     public function up()
     {
-        $this->createTable('brand', [
+        $this->createTable('brand',[
             'id' => $this->primaryKey(),
+            'name'=>$this->string(50)->notNull()->comment('名称'),
+            'intro'=>$this->text()->comment('简介'),
+            'logo'=>$this->string()->notNull()->comment('LOGO'),
+            'sort'=>$this->integer()->notNull()->comment('排序'),
+            'is_deleted'=>$this->integer(1)->notNull()->comment('状态')
         ]);
     }
 
@@ -22,15 +27,6 @@ class m180226_111549_create_brand_table extends Migration
      */
     public function down()
     {
-        $this->dropTable('brand',[
-            'id' => $this->primaryKey(),
-            'name'=>$this->string(50)->notNull()->comment('名称'),
-                'intro'=>$this->text()->comment('简介'),
-                'logo'=>$this->string()->notNull()->comment('LOGO'),
-                'sort'=>$this->integer()->notNull()->comment('排序'),
-                'is_deleted'=>$this->integer(1)->notNull()->comment('状态')
-            ]
-
-        );
+        $this->dropTable('brand');
     }
 }
